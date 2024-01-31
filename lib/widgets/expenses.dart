13939acdca11e0bfcbc2b73aa204d6261d525d01
brighttorchstudio <brighttorchstudio@gmail.com>
2024-01-31 10:21:19 +0700

    clear snack bars

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -46,6 +46,7 @@ class _ExpensesState extends State<Expenses> {
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         duration: Duration(seconds: 3),
