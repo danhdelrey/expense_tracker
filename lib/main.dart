419315fe4a31ff4d89fa2972498c60406b2cd3a1
brighttorchstudio <brighttorchstudio@gmail.com>
2024-01-31 10:33:59 +0700

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: Colors.pinkAccent),
       home: Expenses(),
     ),
   );
